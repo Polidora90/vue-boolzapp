a6acb05f7a5aeeb8bc049fa801ec1de9fef62072
Polidora90 <alessandra.torry@gmail.com>
2021-05-05 15:19:53 +0200

    milestone 3 work in progress

--- scripts/app.js
+++ scripts/app.js
@@ -2,11 +2,30 @@ const app = new Vue({
     el: "#app",
     data: {
         usersList: globalUsersList,
-        activeUser: "",
+        activeUser: {},
+        myMessage: ""
     },
     methods: {
         selectUserChat(user) {
             this.activeUser = user;
+        },
+        formatTime(stringDate) {
+            return moment(stringDate, "DD/MM/YYYY HH:mm:ss").format("HH:mm");
+        },
+        sendMsg() {
+            this.activeUser.messages.push({
+                date: moment(),
+                text: this.myMessage,
+                status: 'sent'
+            });
+            setTimeout(function(){
+                this.activeUser.messages.push({
+                    date: moment(),
+                    text: "Ok!",
+                    status: 'received'
+                });
+            }, 1000);
+
         }
     },
     mounted() {
